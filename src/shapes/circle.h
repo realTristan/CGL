a4c5dbccd899fcbee353af5bd1af45d86df0b71a
tristan <heytristaann@gmail.com>
2023-07-18 07:36:26 -0400

    more funcs

--- src/shapes/circle.h
+++ src/shapes/circle.h
@@ -8,6 +8,10 @@
 #include <GLFW/glfw3.h>
 #include <math.h>
 
+/**
+ * @brief A class that represents a circle.
+ * 
+ */
 class Circle
 {
 private:
@@ -32,49 +36,91 @@ public:
         this->update_vertices();
     }
 
-<<<<<<< HEAD
-    Vec3D<GLfloat> get_position()
-    {
-        return this->position;
-    }
-
-    GLfloat get_radius()
-    {
-        return this->radius;
-    }
-
+    /**
+     * @brief Gets the fill color of the circle.
+     *
+     * @return The fill color of the circle.
+     */
     Color get_fill_color()
     {
         return this->fill_color;
     }
 
+    /**
+     * @brief Gets the outline color of the circle.
+     *
+     * @return The outline color of the circle.
+     */
     Color get_outline_color()
     {
         return this->outline_color;
     }
 
+    /**
+     * @brief Gets the outline width of the circle.
+     *
+     * @return The outline width of the circle.
+     */
     GLfloat get_outline_width()
     {
-        return this->width;
+        return this->outline_width;
     }
 
+    /**
+     * @brief Gets the number of sides of the circle.
+     *
+     * @return The number of sides of the circle.
+     */
     GLint get_sides()
     {
         return this->sides;
     }
 
+    /**
+     * @brief Gets the position of the circle.
+     *
+     * @return The position of the circle.
+     */
+    Vec3D<GLfloat> get_position()
+    {
+        return this->position;
+    }
+
+    /**
+     * @brief Gets the radius of the circle.
+     *
+     * @return The radius of the circle.
+     */
+    GLfloat get_radius()
+    {
+        return this->radius;
+    }
+
+    /**
+     * @brief Gets the vertices of the circle.
+     *
+     * @return The vertices of the circle.
+     */
+    GLfloat *get_vertices()
+    {
+        return this->vertices;
+    }
+
+    /**
+     * @brief Gets the number of vertices of the circle.
+     *
+     * @return The number of vertices of the circle.
+     */
     GLint get_num_of_vertices()
     {
         return this->num_of_vertices;
     }
 
-=======
     /**
      * @brief Sets the fill color of the circle.
      *
      * @param color The color to set the fill color to.
      */
->>>>>>> 5053d5bc34154014a26df1f9f5539166c28b368c
     void set_fill_color(Color color)
     {
         this->fill_color = color;
